657b818037aacbb71478522d8114498d1be1698e
xiongda112312 <dax86931@gmail.com>
2024-07-22 14:21:05 +0800

    submit to

---
+++ p2pnetwork/__init__.py
@@ -0,0 +1,16 @@
+#######################################################################################################################
+# Author: Maurice Snoeren <macsnoeren(at)gmail.com>                                                                   #
+# Version: 0.1 beta (use at your own risk)                                                                            #
+#                                                                                                                     #
+# Package p2pnet for decentralized peer-to-peer network applications                                                  #
+#######################################################################################################################
+
+__title__ = 'python-p2p-network'
+__author__ = 'Maurice Snoeren'
+__license__ = "GNU 3.0"
+__main__ = "p2pnetwork"
+
+__all__ = ["node", "Node", "nodeconnection", "NodeConnection"]
+
+from .node import Node
+from .nodeconection import NodeConnection
